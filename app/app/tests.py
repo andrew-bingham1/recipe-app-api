1f6b0256210c94b0878a8a7225d0cc6a75e49d72
Andrew Bingham <andrewgbingham@gmail.com>
2023-09-05 15:12:49 -0600

    fix: correct linting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,14 +9,15 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test Calc Module"""
 
+
     def test_add_numbers(self):
         """Test that two numbers are added together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test that values are subtracted and returned"""
-        res = calc.subtract(5,11)
+        res = calc.subtract(5, 11)
 
-        self.assertEqual(res,6)
+        self.assertEqual(res, 6)
